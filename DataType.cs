36cb6d6967c0c5453c0dabe503829c4ceffc469a
Atikur Rahman <54862457+atik-1010@users.noreply.github.com>
2026-05-19 18:12:50 +0600

    datatype
    datatype

---
+++ DataType.cs
@@ -0,0 +1,52 @@
+using System;
+class DataType
+{
+    static void Main()
+    {
+        // Integer data type
+        int Number = 100;
+        Console.WriteLine(Number);
+
+        //Floating-point data type
+        float FloatNumber = 3.14f;
+        Console.WriteLine(FloatNumber);
+
+        //Double data type
+        double DoubleNumber = 3.14159265358;
+        Console.WriteLine(DoubleNumber);
+
+
+
+        //Character data type
+        char Character = 'A';
+        Console.WriteLine(Character);
+
+        //String data type
+        string Text = "Hello, World!";
+        Console.WriteLine(Text);
+
+
+        //Boolean data type
+        bool Boolean = true;
+        Console.WriteLine(Boolean);
+
+        //Object data type
+        object ObjectData = 10; //boxing an integer into an object
+        Console.WriteLine(ObjectData);
+
+        // Decimal data type
+        decimal DecimalNumber = 19.99m;
+        Console.WriteLine(DecimalNumber);
+
+        //array data type
+        int[] Array = { 1, 2, 3, 4, 5 };
+        Console.WriteLine(Array[0]); // Output: 1   
+
+
+
+
+
+
+
+    }
+}
